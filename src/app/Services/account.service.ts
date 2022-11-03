3c0dd4e6863eb10426ddacfa86b0de17215cf11b
douar <mouadouarzong@gmail.com>
2022-11-03 15:10:51 -0500

    inject httpService into account service inject httpClient into http service

--- src/app/Services/account.service.ts
+++ src/app/Services/account.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import {HttpService} from "./http.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 }
